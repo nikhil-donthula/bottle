42abac1274535f5b9b9cd5c2a90f716ea099d26f
nikhil-donthula <donthulanikhil.com>
2024-01-19 17:38:29 +0530

    Adding test classes

---
+++ src/test/java/com/cool/bottle/BottleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cool.bottle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BottleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
