673cca89234d10c35f979a47cb83e3f7ba982efe
nikhil-donthula <donthulanikhil.com>
2024-01-26 19:18:52 +0530

    Adding Addition service

---
+++ src/main/java/com/cool/bottle/Numbers.java
@@ -0,0 +1,26 @@
+package com.cool.bottle;
+
+
+public class Numbers {
+    private int num1;
+    private int num2;
+    
+    // Getters
+    public int getNum1() {
+        return num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    // Setters
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+    
+}
